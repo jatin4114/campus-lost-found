0547ff9aa0df47a65017246e8fc23d645839855a
jatin4114 <jatinpanghal007@gmail.com>
2022-07-19 11:38:00 +0530

    perf: code-split admin and messaging routes
    Lazy-load the admin section and the messages/conversation pages
    (pulling in socket.io-client) via React.lazy + Suspense — most users
    never open either, so they shouldn't ship in the initial bundle.
    Clears the >500kB chunk-size warning from the production build.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ProtectedRoute } from './components/common/ProtectedRoute'
 import { RequireRole } from './components/common/RequireRole'
-import { AdminLayout } from './components/layout/AdminLayout'
 import { AppLayout } from './components/layout/AppLayout'
 import { AuthProvider } from './context/AuthContext'
-import { AdminAuditLogsPage } from './pages/admin/AdminAuditLogsPage'
-import { AdminDashboardPage } from './pages/admin/AdminDashboardPage'
-import { AdminReportsPage } from './pages/admin/AdminReportsPage'
-import { AdminUsersPage } from './pages/admin/AdminUsersPage'
 import { LoginPage } from './pages/auth/LoginPage'
 import { RegisterPage } from './pages/auth/RegisterPage'
 import { VerifyEmailPage } from './pages/auth/VerifyEmailPage'
@@ -19,44 +15,59 @@ import { MyReportsPage } from './pages/items/MyReportsPage'
 import { ReportItemPage } from './pages/items/ReportItemPage'
 import { LandingPage } from './pages/LandingPage'
 import { MatchesPage } from './pages/MatchesPage'
-import { ConversationPage } from './pages/messages/ConversationPage'
-import { MessagesPage } from './pages/messages/MessagesPage'
 import { NotificationsPage } from './pages/NotificationsPage'
 
+// Code-split the admin section and the messaging/socket.io bundle — most
+// users (students) never open either, so there's no reason to ship them in
+// the initial bundle.
+const AdminLayout = lazy(() => import('./components/layout/AdminLayout').then((m) => ({ default: m.AdminLayout })))
+const AdminDashboardPage = lazy(() => import('./pages/admin/AdminDashboardPage').then((m) => ({ default: m.AdminDashboardPage })))
+const AdminUsersPage = lazy(() => import('./pages/admin/AdminUsersPage').then((m) => ({ default: m.AdminUsersPage })))
+const AdminReportsPage = lazy(() => import('./pages/admin/AdminReportsPage').then((m) => ({ default: m.AdminReportsPage })))
+const AdminAuditLogsPage = lazy(() => import('./pages/admin/AdminAuditLogsPage').then((m) => ({ default: m.AdminAuditLogsPage })))
+const MessagesPage = lazy(() => import('./pages/messages/MessagesPage').then((m) => ({ default: m.MessagesPage })))
+const ConversationPage = lazy(() => import('./pages/messages/ConversationPage').then((m) => ({ default: m.ConversationPage })))
+
 const queryClient = new QueryClient()
+
+function PageFallback() {
+  return <p className="p-8 text-center text-slate-500">Loading…</p>
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <AuthProvider>
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/verify-email" element={<VerifyEmailPage />} />
-              <Route path="/items" element={<ItemsPage />} />
-              <Route path="/items/:id" element={<ItemDetailPage />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path="/dashboard" element={<DashboardPage />} />
-                <Route path="/report" element={<ReportItemPage />} />
-                <Route path="/my-reports" element={<MyReportsPage />} />
-                <Route path="/matches" element={<MatchesPage />} />
-                <Route path="/notifications" element={<NotificationsPage />} />
-                <Route path="/messages" element={<MessagesPage />} />
-                <Route path="/messages/:conversationId" element={<ConversationPage />} />
-                <Route element={<RequireRole roles={['ADMIN', 'MODERATOR']} />}>
-                  <Route path="/admin" element={<AdminLayout />}>
-                    <Route index element={<AdminDashboardPage />} />
-                    <Route path="users" element={<AdminUsersPage />} />
-                    <Route path="reports" element={<AdminReportsPage />} />
-                    <Route path="audit-logs" element={<AdminAuditLogsPage />} />
+          <Suspense fallback={<PageFallback />}>
+            <Routes>
+              <Route element={<AppLayout />}>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/verify-email" element={<VerifyEmailPage />} />
+                <Route path="/items" element={<ItemsPage />} />
+                <Route path="/items/:id" element={<ItemDetailPage />} />
+                <Route element={<ProtectedRoute />}>
+                  <Route path="/dashboard" element={<DashboardPage />} />
+                  <Route path="/report" element={<ReportItemPage />} />
+                  <Route path="/my-reports" element={<MyReportsPage />} />
+                  <Route path="/matches" element={<MatchesPage />} />
+                  <Route path="/notifications" element={<NotificationsPage />} />
+                  <Route path="/messages" element={<MessagesPage />} />
+                  <Route path="/messages/:conversationId" element={<ConversationPage />} />
+                  <Route element={<RequireRole roles={['ADMIN', 'MODERATOR']} />}>
+                    <Route path="/admin" element={<AdminLayout />}>
+                      <Route index element={<AdminDashboardPage />} />
+                      <Route path="users" element={<AdminUsersPage />} />
+                      <Route path="reports" element={<AdminReportsPage />} />
+                      <Route path="audit-logs" element={<AdminAuditLogsPage />} />
+                    </Route>
                   </Route>
                 </Route>
               </Route>
-            </Route>
-          </Routes>
+            </Routes>
+          </Suspense>
         </AuthProvider>
       </BrowserRouter>
     </QueryClientProvider>
